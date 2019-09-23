70d18b350506ad1a7f2b70d2df24abd42cc7745b
zjx <1735429225@qq.com>
2019-09-23 18:01:41 +0800

    new

---
+++ project/Selenium.py
@@ -0,0 +1,179 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import base64
+import random
+from PIL import Image
+from project.spider import Spider
+from lib.log import LogHandler
+log = LogHandler(__name__)
+
+
+class Verification:
+
+    def __init__(self):
+        self.url = 'https://ehire.51job.com/'
+        self.driver = webdriver.Chrome()
+        self.wait = WebDriverWait(self.driver, 20)
+
+    def start(self):
+        """
+        启动
+        :return:
+        """
+        # 打开网页
+        self.driver.get(self.url)
+        # 最大化
+        self.driver.maximize_window()
+        time.sleep(2)
+        # 获取第一个输入框，输入会员名
+        self.driver.find_element_by_id('txtMemberNameCN').send_keys('上海丰草')
+        # 获取第二个输入框，输入用户名
+        self.driver.find_element_by_id('txtUserNameCN').send_keys('上海丰草文化传播有限公司')
+        # 获取第三个输入框，输入密码
+        self.driver.find_element_by_id('txtPasswordCN').send_keys('denglimin11')
+        # 点击登录
+        self.driver.find_element_by_id('Login_btnLoginCN').click()
+        time.sleep(5)
+        self.move()
+
+    def move(self):
+        # 刷新一下验证码
+        self.driver.find_element_by_xpath('//a[@class="geetest_refresh_1"]').click()
+        time.sleep(1)
+        # 保存有缺口的图片
+        self.save_img('cut.png', 'geetest_canvas_bg')
+        # 保存没有缺口的图片
+        self.save_img('full.png', 'geetest_canvas_fullbg')
+        cut_img = Image.open('cut.png')
+        full_img = Image.open('full.png')
+        # 计算距离 需要减去距离左边的间隙
+        distance = self.get_distance(cut_img, full_img) - random.randint(6, 9)
+        # 开始移动
+        self.drag(distance)
+
+        # 判断是否验证成功
+        try:
+            self.wait.until(EC.presence_of_element_located((By.XPATH, '//div[@class="geetest_slider geetest_success"]')))
+            log.info('验证成功')
+        except TimeoutException:
+            log.error('再试一次')
+            time.sleep(5)
+            try:
+                self.driver.find_element_by_xpath('geetest_panel_error_content').click()
+                time.sleep(5)
+                self.move()
+            except TimeoutException:
+                self.move()
+
+        # 获取cookie
+        self.get_cookie()
+        # 关闭浏览器
+        self.driver.close()
+
+    def save_img(self, img_name, class_name):
+        """
+        获取验证码图片
+        :param img_name: 图片名称
+        :param class_name: xpath选择器
+        :return:
+        """
+        # 执行js 获取网页上的base64编码图片，解码成bytes类型后保存图片
+        getImgJS = 'return document.getElementsByClassName("' + class_name + '")[0].toDataURL("image/png");'
+        img = self.driver.execute_script(getImgJS)
+        base64_data_img = img[img.find(',') + 1:]
+        image_base = base64.b64decode(base64_data_img)
+        file = open(img_name, 'wb')
+        file.write(image_base)
+        file.close()
+
+    def get_distance(self, cut_img, full_img):
+        """
+        计算缺口距离
+        :param cut_img: 有缺口的图片
+        :param full_img: 无缺口的图片
+        :return: 缺口距离
+        """
+        for x in range(cut_img.width):
+            for y in range(cut_img.height):
+                cpx = cut_img.getpixel((x, y))
+                fpx = full_img.getpixel((x, y))
+                if not self.is_similar_color(cpx, fpx):
+                    img = cut_img.crop((x, y, x + 50, y + 50))
+                    img.save('gap.png')
+                    return x
+
+    @staticmethod
+    def is_similar_color(x_pixel, y_pixel):
+        """
+        判断两张图片的像素是否相同
+        :param x_pixel: 有缺口的图片像素元组
+        :param y_pixel: 无缺口的图片像素元组
+        :return: True or False
+        """
+        for i, pixel in enumerate(x_pixel):
+            if abs(y_pixel[i] - pixel) > 50:
+                return False
+        return True
+
+    def get_slider(self):
+        """
+        获取滑块
+        :return: 滑块对象
+        """
+        slider = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'geetest_slider_button')))
+        return slider
+
+    def drag(self, distance):
+        """
+        开始移动
+        :param distance: 缺口距离
+        :return:
+        """
+        # 获取滑块
+        slider = self.get_slider()
+        # 按下鼠标左键
+        ActionChains(self.driver).click_and_hold(slider).perform()
+        time.sleep(1)
+        while distance > 0:
+            if distance > 10:
+                span = random.randint(5, 8)
+            else:
+                span = random.randint(2, 3)
+            ActionChains(self.driver).move_by_offset(span, 0).perform()
+            distance -= span
+            time.sleep(random.randint(10, 50) / 100)
+        ActionChains(self.driver).move_by_offset(distance, 1).perform()
+        # 释放鼠标
+        ActionChains(self.driver).release(on_element=slider).perform()
+
+    def get_cookie(self):
+        """
+        获取cookie
+        :return:
+        """
+        cookie = self.driver.get_cookies()
+        time.sleep(5)
+        for i in cookie:
+            self.driver.add_cookie(i)
+        self.driver.get('https://ehire.51job.com/InboxResume/InboxRecentEngine.aspx?Style=1')
+        time.sleep(5)
+        cookies = self.driver.get_cookies()
+        str_cookie = ''
+        for j in cookies:
+            name = j['name']
+            value = j['value']
+            str_cookie = str_cookie + name + '=' + value + ';'
+        log.info(str_cookie)
+        # 将cookie传入爬虫代码
+        s = Spider(str_cookie)
+        s.start()
+
+
+if __name__ == '__main__':
+    v = Verification()
+    v.start()
--- project/spider.py
+++ project/spider.py
@@ -23,6 +23,10 @@ class Spider:
         self.count = 0
 
     def start(self):
+        """
+        启动爬虫
+        :return:
+        """
         r = requests.get(url=self.start_url, headers=self.headers)
         tree = etree.HTML(r.text)
         # 获取第二页的关键参数VIEWSTART
@@ -35,7 +39,7 @@ class Spider:
             user_id = link.xpath('@value1')[0]
             # todo 更加ID 去重
             url = 'https://ehire.51job.com/Candidate/ResumeViewFolderV2.aspx?hidSeqID=' + user_id + '&hidFolder=EMP&pageCode=24'
-            print(url)
+            log.info(url)
             # todo 请求解析
             p = Parse(self.cookie)
             p.start('https://ehire.51job.com/Candidate/ResumeViewFolderV2.aspx', user_id)
@@ -43,8 +47,14 @@ class Spider:
         self.count += 1
         self.small_six_page_params(VIEWSTART, 2, int(max_page))
 
-    # 页码小于6的请求参数
     def small_six_page_params(self, VIEWSTART, page, max_page):
+        """
+        页码小于6的请求参数
+        :param VIEWSTART: 参数
+        :param page: 页码
+        :param max_page: 最大页码
+        :return:
+        """
         data = {
             '__EVENTTARGET': 'pagerBottomNew$btnNum' + str(page),
             '__VIEWSTATE': VIEWSTART,
@@ -53,8 +63,14 @@ class Spider:
         }
         self.request_next_page(data, page, max_page)
 
-    # 页码大于6的请求参数
     def large_six_page_params(self, VIEWSTART, page, max_page):
+        """
+        页码大于6的请求参数
+        :param VIEWSTART: 参数
+        :param page: 页码
+        :param max_page: 最大页码
+        :return:
+        """
         data = {
             '__EVENTTARGET': 'pagerBottomNew$btnNum4',
             '__VIEWSTATE': VIEWSTART,
@@ -64,6 +80,13 @@ class Spider:
         self.request_next_page(data, page, max_page)
 
     def request_next_page(self, data, page, max_page):
+        """
+        请求下一页
+        :param data: 请求参数
+        :param page: 页码
+        :param max_page: 最大页码
+        :return:
+        """
         r = requests.post(url=self.start_url, headers=self.headers, data=data)
         tree = etree.HTML(r.text)
         # 获取下一页的关键参数VIEWSTART
@@ -74,12 +97,12 @@ class Spider:
             user_id = link.xpath('@value1')[0]
             # todo 根据id 去重
             url = 'https://ehire.51job.com/Candidate/ResumeViewFolderV2.aspx?hidSeqID=' + user_id + '&hidFolder=EMP&pageCode=24'
-            print(url)
-            # todo 请求解析
+            log.info(url)
+            # 请求解析
             p = Parse(self.cookie)
             p.start('https://ehire.51job.com/Candidate/ResumeViewFolderV2.aspx', user_id)
-            time.sleep(1)
-        print(VIEWSTART)
+            time.sleep(5)
+        log.info(VIEWSTART)
         # 延迟请求，防止被封IP
         time.sleep(5)
         self.count += 1
@@ -95,6 +118,6 @@ class Spider:
 
 
 if __name__ == '__main__':
-    cookie = 'guid=6cfadb91bfb463e76a875d9012ee90ef; nsearch=jobarea%3D%26%7C%26ord_field%3D%26%7C%26recentSearch0%3D%26%7C%26recentSearch1%3D%26%7C%26recentSearch2%3D%26%7C%26recentSearch3%3D%26%7C%26recentSearch4%3D%26%7C%26collapse_expansion%3D; ps=needv%3D0; _ujz=MTUzNjE4NTk1MA%3D%3D; 51job=cuid%3D153618595%26%7C%26cusername%3Dphone_15996163721_201903126789%26%7C%26cpassword%3D%26%7C%26cname%3D%25D5%25C5%25BD%25F0%25D0%25A4%26%7C%26cemail%3D1735429225%2540qq.com%26%7C%26cemailstatus%3D0%26%7C%26cnickname%3D%26%7C%26ccry%3D.0BXdfJgAFuI.%26%7C%26cconfirmkey%3D176PQZKsaFYdM%26%7C%26cautologin%3D1%26%7C%26cenglish%3D0%26%7C%26sex%3D0%26%7C%26cnamekey%3D17IYfSY3xlHpg%26%7C%26to%3Dfed7de1ed92e5b4ccd97ebf566f02ad35d7a441d%26%7C%26; search=jobarea%7E%60020000%2C070300%7C%21ord_field%7E%600%7C%21recentSearch0%7E%60020000%2C070300%A1%FB%A1%FA000000%A1%FB%A1%FA0000%A1%FB%A1%FA00%A1%FB%A1%FA99%A1%FB%A1%FA%A1%FB%A1%FA99%A1%FB%A1%FA99%A1%FB%A1%FA99%A1%FB%A1%FA99%A1%FB%A1%FA9%A1%FB%A1%FA99%A1%FB%A1%FA%A1%FB%A1%FA0%A1%FB%A1%FA%C9%CF%BA%A3%B7%E1%B2%DD%CE%C4%BB%AF%B4%AB%B2%A5%D3%D0%CF%DE%B9%AB%CB%BE%A1%FB%A1%FA2%A1%FB%A1%FA1%7C%21recentSearch1%7E%60020000%2C070300%A1%FB%A1%FA000000%A1%FB%A1%FA0000%A1%FB%A1%FA00%A1%FB%A1%FA99%A1%FB%A1%FA%A1%FB%A1%FA99%A1%FB%A1%FA99%A1%FB%A1%FA99%A1%FB%A1%FA99%A1%FB%A1%FA9%A1%FB%A1%FA99%A1%FB%A1%FA%A1%FB%A1%FA0%A1%FB%A1%FA%C5%C0%B3%E6%A1%FB%A1%FA2%A1%FB%A1%FA1%7C%21recentSearch2%7E%60020000%2C070300%A1%FB%A1%FA000000%A1%FB%A1%FA0000%A1%FB%A1%FA00%A1%FB%A1%FA02%A1%FB%A1%FA%A1%FB%A1%FA99%A1%FB%A1%FA99%A1%FB%A1%FA99%A1%FB%A1%FA99%A1%FB%A1%FA9%A1%FB%A1%FA99%A1%FB%A1%FA%A1%FB%A1%FA0%A1%FB%A1%FA%C5%C0%B3%E6%A1%FB%A1%FA2%A1%FB%A1%FA1%7C%21recentSearch3%7E%60020000%2C070300%A1%FB%A1%FA000000%A1%FB%A1%FA0000%A1%FB%A1%FA00%A1%FB%A1%FA99%A1%FB%A1%FA%A1%FB%A1%FA99%A1%FB%A1%FA99%A1%FB%A1%FA99%A1%FB%A1%FA99%A1%FB%A1%FA9%A1%FB%A1%FA99%A1%FB%A1%FA%A1%FB%A1%FA0%A1%FB%A1%FApython%C5%C0%B3%E6%A1%FB%A1%FA2%A1%FB%A1%FA1%7C%21recentSearch4%7E%60020000%2C070300%A1%FB%A1%FA000000%A1%FB%A1%FA0000%A1%FB%A1%FA00%A1%FB%A1%FA99%A1%FB%A1%FA%A1%FB%A1%FA99%A1%FB%A1%FA99%A1%FB%A1%FA99%A1%FB%A1%FA99%A1%FB%A1%FA9%A1%FB%A1%FA99%A1%FB%A1%FA%A1%FB%A1%FA0%A1%FB%A1%FApython%A1%FB%A1%FA2%A1%FB%A1%FA1%7C%21collapse_expansion%7E%601%7C%21; adv=adsnew%3D0%26%7C%26adsnum%3D2004282%26%7C%26adsresume%3D1%26%7C%26adsfrom%3Dhttps%253A%252F%252Fsp0.baidu.com%252F9q9JcDHa2gU2pMbgoY3K%252Fadrc.php%253Ft%253D06KL00c00fDewkY0KKG-00uiAsjjuLkI000007jzr-C00000LXA5xZ.THYdnyGEm6K85yF9pywd0ZnquHwBPW0zm10snj0dmvnkPsKd5RRdnbnsn1Fjrj6vrHb3njFAwj-afHN7wH6sP1PDnYRd0ADqI1YhUyPGujY1nWT4njbsPHTdFMKzUvwGujYkP6K-5y9YIZK1rBtEIZF9mvR8PH7JUvc8mvqVQLwzmyP-QMKCTjq9uZP8IyYqnW0sPjc3nBu9pM0qmR9inAPcHHunXH-YmHPwIR4RwM7Bnb-dyHc4IDs1Rh4nnY4_m-n4IvN_rZ-PwDRYHAdCnAFgIzqpUbGvm-fkpN-gUAVbyDcvFh_qn1u-njRYmyDsPjR4m1F-uAfdmWfLuWRLmvcvnHKbrjc0mLFW5HfLn1bd%2526tpl%253Dtpl_11534_19968_16032%2526l%253D1513812560%2526attach%253Dlocation%25253D%252526linkName%25253D%252525E6%252525A0%25252587%252525E5%25252587%25252586%252525E5%252525A4%252525B4%252525E9%25252583%252525A8-%252525E6%252525A0%25252587%252525E9%252525A2%25252598-%252525E4%252525B8%252525BB%252525E6%252525A0%25252587%252525E9%252525A2%25252598%252526linkText%25253D%252525E3%25252580%25252590%252525E5%25252589%2525258D%252525E7%252525A8%2525258B%252525E6%25252597%252525A0%252525E5%252525BF%252525A751Job%252525E3%25252580%25252591-%25252520%252525E5%252525A5%252525BD%252525E5%252525B7%252525A5%252525E4%252525BD%2525259C%252525E5%252525B0%252525BD%252525E5%2525259C%252525A8%252525E5%25252589%2525258D%252525E7%252525A8%2525258B%252525E6%25252597%252525A0%252525E5%252525BF%252525A7%2521%252526xp%25253Did%2528%25252522m3279090575_canvas%25252522%2529%2525252FDIV%2525255B1%2525255D%2525252FDIV%2525255B1%2525255D%2525252FDIV%2525255B1%2525255D%2525252FDIV%2525255B1%2525255D%2525252FDIV%2525255B1%2525255D%2525252FH2%2525255B1%2525255D%2525252FA%2525255B1%2525255D%252526linkType%25253D%252526checksum%25253D220%2526ie%253DUTF-8%2526f%253D8%2526tn%253Dbaidu%2526wd%253D51job%2526oq%253D51job%2526rqlang%253Dcn; EhireGuid=c0ac2273169c47e3ab7dffab34678ba6; LangType=Lang=&Flag=1; RememberLoginInfo=member_name=42C0E5A4273247A0CA6AD9118CC10153&user_name=42C0E5A4273247A0BE39E05CE3F038B97F346A4FD26D49881290FBC4487FA1C3; ASP.NET_SessionId=023dciai3c5nbdcybq53aym1; AccessKey=5ff3aa3dae5d4b5; HRUSERINFO=CtmID=4371481&DBID=1&MType=02&HRUID=5809286&UserAUTHORITY=1111111111&IsCtmLevle=1&UserName=%e4%b8%8a%e6%b5%b7%e4%b8%b0%e8%8d%89%e6%96%87%e5%8c%96%e4%bc%a0%e6%92%ad%e6%9c%89%e9%99%90%e5%85%ac%e5%8f%b8&IsStandard=0&LoginTime=09%2f21%2f2019+14%3a26%3a26&ExpireTime=09%2f21%2f2019+14%3a36%3a26&CtmAuthen=0000011000000001000110010000000011100001&BIsAgreed=true&IsResetPwd=0&CtmLiscense=1&AccessKey=fc01943ccde97937&source=0'
+    cookie = 'adv=adsnew%3D1%26%7C%26adsresume%3D1%26%7C%26adsfrom%3Dhttps%253A%252F%252Fsp0.baidu.com%252F9q9JcDHa2gU2pMbgoY3K%252Fadrc.php%253Ft%253D06KL00c00fDewkY0KKG-00uiAsjdy3wI000007jzr-C00000xDd5wZ.THYdnyGEm6K85yF9pywd0ZnqujmvuymsuAnsnj7bmhRsP6Kd5RDzfWm3fRPAnWTzPDndnbDvnRNjfYcLPYR4fWuAfb7A0ADqI1YhUyPGujY1nWT4njbsPHTdFMKzUvwGujYkP6K-5y9YIZK1rBtEIZF9mvR8PH7JUvc8mvqVQLwzmyP-QMKCTjq9uZP8IyYqnW0sPjc3nBu9pM0qmR9inAPcHHunXH-YmHPwIR4RwM7Bnb-dyHc4IDs1Rh4nnY4_m-n4IvN_rZ-PwDRYHAdCnAFgIzqpUbGvm-fkpN-gUAVbyDcvFh_qn1u-njRYmyDsPjR4m1F-uAfdmWfLuWRLmvcvnHKbrjc0mLFW5HnsP1f4%2526tpl%253Dtpl_11534_19968_16032%2526l%253D1513812560%2526attach%253Dlocation%25253D%252526linkName%25253D%252525E6%252525A0%25252587%252525E5%25252587%25252586%252525E5%252525A4%252525B4%252525E9%25252583%252525A8-%252525E6%252525A0%25252587%252525E9%252525A2%25252598-%252525E4%252525B8%252525BB%252525E6%252525A0%25252587%252525E9%252525A2%25252598%252526linkText%25253D%252525E3%25252580%25252590%252525E5%25252589%2525258D%252525E7%252525A8%2525258B%252525E6%25252597%252525A0%252525E5%252525BF%252525A751Job%252525E3%25252580%25252591-%25252520%252525E5%252525A5%252525BD%252525E5%252525B7%252525A5%252525E4%252525BD%2525259C%252525E5%252525B0%252525BD%252525E5%2525259C%252525A8%252525E5%25252589%2525258D%252525E7%252525A8%2525258B%252525E6%25252597%252525A0%252525E5%252525BF%252525A7%2521%252526xp%25253Did%2528%25252522m3279090575_canvas%25252522%2529%2525252FDIV%2525255B1%2525255D%2525252FDIV%2525255B1%2525255D%2525252FDIV%2525255B1%2525255D%2525252FDIV%2525255B1%2525255D%2525252FDIV%2525255B1%2525255D%2525252FH2%2525255B1%2525255D%2525252FA%2525255B1%2525255D%252526linkType%25253D%252526checksum%25253D220%2526ie%253DUTF-8%2526f%253D8%2526tn%253Dbaidu%2526wd%253D51job%2526rqlang%253Dcn%26%7C%26adsnum%3D2004282; guid=fa1f7d11e1e6c06f95cf8c6fcc300d52; EhireGuid=ef2f1a33bfbd47cbaf3a5f5dd2d8eaa8; LangType=Lang=&Flag=1; RememberLoginInfo=member_name=42C0E5A4273247A0CA6AD9118CC10153&user_name=42C0E5A4273247A0BE39E05CE3F038B97F346A4FD26D49881290FBC4487FA1C3; ASP.NET_SessionId=wohboelsnfjdoczktp3xe5p4; AccessKey=b892042e15f04ed; HRUSERINFO=CtmID=4371481&DBID=1&MType=02&HRUID=5809286&UserAUTHORITY=1111111111&IsCtmLevle=1&UserName=%e4%b8%8a%e6%b5%b7%e4%b8%b0%e8%8d%89%e6%96%87%e5%8c%96%e4%bc%a0%e6%92%ad%e6%9c%89%e9%99%90%e5%85%ac%e5%8f%b8&IsStandard=0&LoginTime=09%2f23%2f2019+09%3a45%3a18&ExpireTime=09%2f23%2f2019+09%3a55%3a18&CtmAuthen=0000011000000001000110010000000011100001&BIsAgreed=true&IsResetPwd=0&CtmLiscense=1&AccessKey=c6abeec633f95480&source=0'
     s = Spider(cookie)
     s.start()
